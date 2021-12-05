d2f71895fe062d48977f3ac370f39319c471aab2
Mattias Hermansson <hexagenic@gmail.com>
2021-12-05 17:35:40 +0100

    Day 1a slightly faster

--- src/solutions/day1.rs
+++ src/solutions/day1.rs
@@ -23,13 +23,8 @@ pub fn part_b(file: &str) -> i64 {
 
     let mut count = 0;
 
-    let a_it = lines.iter();
-    let b_it = lines.iter().skip(1);
-    let c_it = lines.iter().skip(2);
-    let rolling_window = a_it.zip(b_it).zip(c_it);
-
-    for ((a, b), c) in rolling_window {
-        let line = a + b + c;
+    for vals in lines.windows(3) {
+        let line = vals.iter().sum();
         if let Some(last_line) = last_line {
             if line > last_line {
                 count += 1;
